89a04beda8358149d5bc9949d9f8d5b8a9f14564
lukaset649 <setlaklukasz@gmail.com>
2024-12-05 14:20:12 +0100

    Changed output in AddReservation to return reservation_details_id instead of reservation_id. Added example of use

--- photoservice_procedures.sql
+++ photoservice_procedures.sql
@@ -1,66 +1,47 @@
 --PROCEDURY
 --Procedura pozwalaj¹ca na dodanie rezerwacji dla u¿ytkowników z odpowiednimi rolami
+
 CREATE PROCEDURE AddReservation
-	@client_id INT,
-	@service_id INT,
-	@date DATETIME,
-	@other_info VARCHAR(2000),
-	@type_id INT --typ zlecenia z tabeli reservation_details. Detale mog¹ byæ uzupe³nione póŸniej
+    @client_id INT,
+    @service_id INT,
+    @date DATETIME,
+    @other_info VARCHAR(2000),
+    @type_id INT,  -- typ zlecenia z tabeli reservation_details
+    @reservation_details_id INT OUTPUT -- Dodajemy parametr OUTPUT
 AS
 BEGIN
-	--sprawdzenie czy u¿ytkownik ma rolê klienta(id4) albo admina(id1)
-	IF EXISTS (
-		SELECT 1
-		FROM user_role ur
-		JOIN roles r ON ur.role_id = r.id_role
-		WHERE ur.user_id = @client_id AND r.id_role IN (1, 4)
-	)
-	BEGIN
+    -- Sprawdzenie czy u¿ytkownik ma rolê klienta(id4) albo admina(id1)
+    IF EXISTS (
+        SELECT 1
+        FROM user_role ur
+        JOIN roles r ON ur.role_id = r.id_role
+        WHERE ur.user_id = @client_id AND r.id_role IN (1, 4)
+    )
+    BEGIN
 		DECLARE @reservation_id INT;
 
-		--Dodanie rezerwacji
-		INSERT INTO reservation(client_id, service_id, date, other_info)
-		VALUES (@client_id, @service_id, @date, @other_info);
+        -- Dodanie rezerwacji
+        INSERT INTO reservation(client_id, service_id, date, other_info)
+        VALUES (@client_id, @service_id, @date, @other_info);
 
-		--zapisuje id ostatnio dodanej rezerwacji
 		SET @reservation_id = SCOPE_IDENTITY();
 
-		--przypisuje typ zlecenia do rezerwacji (details_id zostanie uzupe³nione póŸniej, w osobnej procedurze wprowadzaj¹cej details)
-		INSERT INTO reservation_details(reservation_id, type_id)
+        -- Przypisanie typu zlecenia do rezerwacji (details_id zostanie uzupe³nione póŸniej)
+        INSERT INTO reservation_details(reservation_id, type_id)
         VALUES (@reservation_id, @type_id);
 
-		PRINT 'Rezerwacja zlecenia zosta³a dodana.';
-	END
-	ELSE
-	BEGIN
-		PRINT 'Brak uprawnieñ do dodania rezerwacji.';
-	END
+		-- Zapisanie ID ostatnio dodanego rekordu
+        SET @reservation_details_id = SCOPE_IDENTITY();
+
+        PRINT 'Rezerwacja zlecenia zosta³a dodana.';
+    END
+    ELSE
+    BEGIN
+        PRINT 'Brak uprawnieñ do dodania rezerwacji.';
+    END
 END;
 
-SELECT * FROM reservation_type
 
-
---WYWO£ANIE PROCEDUR
-EXEC AddReservation 
-    @client_id = 2,         -- ID u¿ytkownika próbuj¹cego dodaæ rezerwacjê (brak uprawnieñ)
-    @service_id = 2,		
-    @date = '2025-08-15', 
-    @other_info = 'Sesja fotograficzna na pla¿y.',
-	@type_id = 1;
-
-EXEC AddReservation 
-    @client_id = 6,         -- ID u¿ytkownika próbuj¹cego dodaæ rezerwacjê (posiada upranienia)
-    @service_id = 1,		
-    @date = '2024-08-15', 
-    @other_info = 'Sesja fotograficzna na pla¿y.',
-	@type_id = 2;
-
-
---SELECT * FROM reservation
---select * from reservation_details
---select * from reservation_type
---SELECT * FROM user_role
---SELECT * FROM users
 
 --Procedura dodania rekordu do tabeli details_wedding i uzupe³nienia details_id w tabeli reservation_details
 CREATE PROCEDURE AddDetailsWedding
@@ -175,3 +156,23 @@ BEGIN
     SET details_id = @details_other_id
     WHERE id_res_det = @id_reservation_details;
 END;
+
+
+--PRZYK£AD WYWO£ANIA
+--aplikacja sprawdza jaki typ zosta³ wybrany i na tej podstawie wywo³uje odpowiedni¹ procedurê (np: case 4: EXEC AddDetailsOther)
+DECLARE @reservation_details_id INT;
+
+-- Wywo³anie procedury AddReservation z przekazaniem parametrów i odbiorem @reservation_id
+EXEC AddReservation
+    @client_id = 6,
+    @service_id = 1,
+    @date = '2024-12-05',
+    @other_info = 'Informacje dodatkowe...',
+    @type_id = 4,
+    @reservation_details_id = @reservation_details_id OUTPUT; --zapisanie id do @reservation_id na wyjœciu
+
+-- Wywo³anie procedury AddDetailsOther z przekazaniem @reservation_id
+EXEC AddDetailsOther
+    @id_reservation_details = @reservation_details_id,
+    @localisation = 'ul. Nowa 5, Wroc³aw',  -- Lokalizacja
+    @description = 'Event w plenerze z widokiem na jezioro';  -- Opis
